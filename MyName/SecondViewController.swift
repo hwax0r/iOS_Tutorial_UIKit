ade7f0d07a225392ebd0f0052211c8d1d2a52622
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-13 23:27:53 +0200

    Implemented "showAlert" method to the button on second view

--- MyName/SecondViewController.swift
+++ MyName/SecondViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func showAlert(){
+        // инициализация всплывающего окна
+        let alertController = UIAlertController(
+            title: "Welcome",
+            message: "This is \"myName\" application",
+            preferredStyle: .alert)
+        
+        // кнопки всплывающего окна
+        let actionOK = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        let actionCancel = UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: nil)
+        
+        // добавление кнопок
+        alertController.addAction(actionOK)
+        alertController.addAction(actionCancel)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
