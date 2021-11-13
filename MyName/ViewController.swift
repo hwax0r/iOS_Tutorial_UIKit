c4182bdcbca492b98670019a5139375239f85e68
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-11-13 18:47:17 +0200

    Initial Commit

---
+++ MyName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by David Sergeev on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
